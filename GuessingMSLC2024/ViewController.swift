183d9529504ca6e4aafc280c8336dfde12898887
Eric Larson <eclarson@smu.edu>
2024-08-26 13:34:13 -0500

    Initial Commit

---
+++ GuessingMSLC2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessingMSLC2024
+//
+//  Created by Eric Cooper Larson on 8/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
