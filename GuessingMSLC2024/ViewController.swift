a895b47ddaf4ab4a4106576704279835d2bd157c
Eric Larson <eclarson@smu.edu>
2024-08-26 14:17:49 -0500

    adding simple interfaces to the app

--- GuessingMSLC2024/ViewController.swift
+++ GuessingMSLC2024/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        helloWorldLabel.text = "Hello MSLC World"
     }
 
 
+    @IBAction func changeText(_ sender: Any) {
+        helloWorldLabel.text = "2024 Is Great!"
+    }
 }
 
