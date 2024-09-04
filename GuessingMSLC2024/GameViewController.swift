a9ee4b11e7304d424b96c355cf1c37202aa02f7e
Eric Larson <eclarson@smu.edu>
2024-09-04 07:57:57 -0500

    added new view controller in class

---
+++ GuessingMSLC2024/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GuessingMSLC2024
+//
+//  Created by Eric Cooper Larson on 8/28/24.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
